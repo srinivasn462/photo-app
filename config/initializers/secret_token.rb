6ce96e6becd36284880c182329f1a93043b64c49
TrailHacker <lucas@hardbarger.com>
2012-09-30 22:13:35 -0400

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoApp::Application.config.secret_token = 'e3f6e8a4763f4fd2de462363bfd6541278b793762bd5fa1e019468c6e2c0e88b8016a73352df2ef7d6a5293a94c71564b7b4cf44608d6a7fb8d803591da7ad57'
